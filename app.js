7f7fe1aa122cfebf9bbe0f304bd91f70ea5754ff
Mykola Hubytskyi <hubytskyi@gmail.com>
2024-07-25 21:35:58 +0300

    add webinar 1

--- app.js
+++ app.js
@@ -1,1 +1,122 @@
-console.log('Hello World!');
+"use strict";
+
+var message;
+
+// let message = "Hello World!";
+// message = 42;
+
+// const message = "Hello!";
+// message = 1;
+// console.log(message);
+
+// const user = "John";
+// const User = "Jane";
+
+// console.log(user);
+// console.log(User);
+
+// const num = 123;
+// const userName = "Jane";
+// let userName = 123
+// const API_URL = "";
+// const userFirstName = "Jane";
+
+// const user_name =
+
+// const years = 30 - 5;
+
+// console.log(Infinity);
+
+// console.log(BigInt(42) + 2n);
+
+// const name = "Анна";
+
+// console.log("Привіт, " + name);
+// console.log(`Привіт, ${name}`);
+
+// console.log(true);
+// console.log(false);
+
+// const user = {
+//   name: "Анна",
+//   age: 20,
+//   isActive: true,
+//   hasPets: false,
+//   address: {
+//     city: "Kyiv",
+//     zip: 12345,
+//   },
+// };
+
+// let user = null;
+
+// logic ...
+
+// user = {};
+// console.log(user);
+
+// if (typeof user === "object" && user !== null) {
+// }
+
+// const number = 11;
+
+// console.log(number % 2);
+
+// console.log(typeof null);
+
+// console.log(10 <= 10);
+// console.log(5 >= 10);
+// console.log(15 >= 10);
+
+// const isAdmin = true;
+// const hasPets = false;
+// const age = 17;
+
+// console.log(isAdmin || hasPets || age >= 18);
+
+// console.log(!!"q");
+
+// let user1 = "Анна";
+// const user2 = user1;
+// user = "Анна";
+
+// user1 = "Юлія";
+// console.log(user1);
+// console.log(user2);
+
+// adiu3y4h23xb6g2x63g2xdg22
+// const user = {
+//   name: "Анна",
+// };
+
+// // iu3axdsdasdg2xdg22
+// const user3 = {
+//   name: "Анна",
+// };
+
+// console.log(user === user3);
+
+// const user2 = user; // user = adiu3y4h23xb6g2x63g2xdg22
+
+// user2.name = "Юлія";
+
+// console.log(user === user2);
+// console.log(user2);
+
+// console.log("---> ", user1, user2);
+// console.log(user2);
+
+// let message = 1;
+// let message = 2;
+
+// var message = 1;
+// var message = 2;
+
+// console.log(message); // undefined
+// console.log(message2); // Error
+// let message2 = 5;
+// var message = 10;
+// console.log(message); // 10
+// console.log(message2); // 5
+
+console.log(typeof "1");
