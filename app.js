b68f4be3db646fb4cd4e2a702586bb3318dbc7a6
Mykola Hubytskyi <hubytskyi@gmail.com>
2024-08-01 21:05:08 +0300

    add functions

--- app.js
+++ app.js
@@ -1,244 +1,171 @@
 "use strict";
 
-const text = "some text";
+// const message = "Привіт!";
 
-// const number = Number(text);
-// const number2 = +text;
+// function showMessage() {
+//   const message = "Hello";
 
-// console.log(typeof text);
-// console.log(typeof number);
-// console.log(number2);
-// console.log(typeof number2);
+//   console.log(message);
+// }
 
-// const number3 = parseFloat("42");
+// функція приймає параметри
+// function showMessage2(name = "незнайомець") {
+//   console.log(`Привіт, ${name}`);
+// }
 
-// const sum = "1000auh";
+// під час виклику функції передаються аргументи
+// showMessage2();
 
-// const parsedSum = parseFloat(sum);
-
-// const number = 100;
-// const convertedToString = number.toString();
-// const convertedToString2 = number + "";
-
-// null.toString(); // error
-// undefined.toString(); // error
-
-// console.log(convertedToString2);
-// console.log(typeof convertedToString2);
-// console.log(typeof convertedToString);
-// console.log(String(100));
-
-// const unknown = undefined;
-// const unknown2 = null;
-// const unknown3 = false;
-// const unknown4 = 0;
-// const unknown5 = NaN;
-// const unknown6 = "";
-
-// const unknown7 = "1";
-// const unknown8 = {};
-// const unknown9 = [];
-
-// const result = Boolean(unknown);
-// const result2 = Boolean(unknown2);
-// const result3 = Boolean(unknown3);
-// const result4 = Boolean(unknown4);
-// const result5 = Boolean(unknown5);
-// const result6 = Boolean(unknown6);
-// const result7 = Boolean(unknown7);
-// const result8 = Boolean(unknown8);
-// const result9 = Boolean(unknown9);
-
+// function getSum(a, b) {
+// const result = a + b;
 // console.log(result);
-// console.log(result2);
-// console.log(result3);
-// console.log(result4);
-// console.log(result5);
-// console.log(result6);
-// console.log(result7);
-// console.log(result8);
-// console.log(result9);
-// console.log(!!unknown9.length);
-// console.log(!!Object.keys(unknown8).length);
-
-// const number = "42";
-// const bool = true;
-// console.log(BigInt(bool));
-
-// console.log(1 + 1 + "1");
-
-// const valueFromAPI = "Юлія";
-// const valueFromAPI = "Юлія";
-// const number = 0;
-
-// const name = valueFromAPI || false || "Unknown";
-// const name2 = valueFromAPI && false && "Unknown";
-
-// const name3 = valueFromAPI ?? "Unknown";
-// const isValidValue = false ?? 42;
-
-// const html = '<h1>Hello</h1>'
-
-// const result = false || true;
-// const result2 = true || false;
-
-// valueFromAPI && html;
-
-// console.log(isValidValue);
-
-// MATH
-
-// console.log(Math.trunc(Math.random() * 100));
-// console.log(Math.floor(2.2));
-// console.log(Math.floor(2.8));
-// console.log(Math.ceil(2.8));
-// console.log(Math.ceil(2.8));
-// console.log(Math.round(2.2));
-// console.log(Math.round(2.8));
-// console.log(Math.round(2.5));
-
-// const number = 42.12345;
-
-// console.log(number.toFixed(2));
-
-// const text2 = "SOme \\ text";
-
-// console.log("text".length);
-// console.log("".length);
-// console.log(text2);
-// console.log(text2[0]);
-// console.log(text2.charAt(0));
-// console.log(text2[text2.length - 1]);
-// console.log(text2.charAt(0));
-
-// console.log(text2.toUpperCase());
-// console.log(text2.toLowerCase());
-
-const text3 = "Вийду на берег Дунаю";
-
-// console.log(text3.indexOf("берег", 10));
-// console.log(text3.includes("берег"));
-
-// console.log(text3.slice(0, 3));
-// console.log(text3.substring(5, 3));
-// console.log(text3.substring(5, 3));
-// console.log(text3.substr(0, 5));
-
-// console.log(text3.trim());
-
-// LOOP
-
-let i = 0;
-// let j = 0;
-
-// while (i < 0) {
-//   console.log("i:", i);
-//   // i = i + 1
-//   // i += 1;
-
-//   i++;
+// return result;
+// return a + b;
 // }
-
-// do {
-//   console.log("j: ", j);
-//   j++;
-// } while (j < 1);
-
-// for (let i = 0; i < text3.length; i++) {
-//   console.log(text3[i]);
-// }
-
-// let sum = 0;
-
-// while (true) {
-//   const value = +prompt("Введіть значення", "");
-
-//   if (!value) {
-//     break;
-//   }
-
-//   // sum = sum + value
-//   sum += value;
-// }
-
-// console.log("SUM: ", sum);
-
-// for (let i = 0; i < 10; i++) {
-//   if (i % 2 !== 0) {
-//     continue;
-//   }
-
-//   console.log(i);
-// }
-
-// const year = +prompt("Коли було реліз ES6?", "");
-
-// if (year > 2015) {
-//   console.log("Запізно");
-// } else if (year < 2015) {
-//   console.log("Зарано");
-// } else {
-//   console.log("Точно!");
-// }
-
-// if () {
-
-// }
-
-// if () {
-
-// }
-
-// if () {
-
-// } else if {
-
-// }
-// if () {
-
-// } else {
-
-// }
-// console.log(year);
-// const age = 20;
-// age >= 18 ? console.log("Більше") : console.log("менше");
-// const isAllowed = age >= 18;
-// console.log(isAllowed);
-
-// const sum = 15;
-
-// if (sum === 10) {
-// } else if (sum === 5) {
-// }
+// const sum = getSum(2, 3);
 
 // const user = {
-//   id: 1,
-//   name: "John",
-//   role: "admin",
+//   firstName: "John",
+//   lastName: "Doe",
 // };
 
-// const USER_ADMIN = "admin";
-// const USER_MODERATOR = "moder";
-// const USER_SUPERADMIN = "superadmin";
-// const USER_CUSTOMER = "customer";
-
-// switch (user.role) {
-//   case USER_ADMIN:
-//     console.log("сума дорівню 5");
-//     break;
-//   case USER_MODERATOR:
-//   case USER_SUPERADMIN:
-//     console.log("сума дорівню 10 або 15");
-//     break;
-//   case USER_CUSTOMER:
-//     console.log("сума 20");
-//     break;
-//   default:
-//     console.log("невідомо");
+// function getFullName(firstName, lastName) {
+//   return `${firstName} ${lastName}`;
 // }
 
-// if (username === 'Юлія' && age >= 18) {
+// const fullName = getFullName(user.firstName, user.lastName);
+// console.log("fullName: ", fullName);
 
-// } else if (username === 'Дмитро' && isAdmin) {
+// function getSum(a, b) {
+//   if (typeof a !== "number" && typeof b !== "number") {
+//     return "Потрібно ввести числа";
+//   } else {
+//     return a + b;
+//   }
+// }
 
+// function getSum(a, b) {
+//   if (typeof a !== "number" && typeof b !== "number") {
+//     return "Потрібно ввести числа";
+//   }
+
+//   return a + b;
+// }
+
+// function getSum(a, b) {
+//   if (typeof a !== "number" && typeof b !== "number") {
+//     console.log("Потрібно ввести числа");
+//   } else {
+//     console.log(a + b);
+//   }
+// }
+
+// getSum(1, 2);
+// console.log(sum);
+
+// function getSum(a, b) {
+//   const result = a + b;
+//   return; // undefined
+// }
+
+// const sum = getSum(1, 2);
+// console.log(sum);
+
+// function getSum(a, b) {
+//   const result = a + b;
+//   return result;
+// }
+
+// if (getSum(2, 2) === 5) {
+//   console.log("Вірно!");
+// } else {
+//   console.log("---");
+// }
+
+// console.log(getSum(1, 2));
+
+// function getSum(a, b) {
+//   const result = a + b;
+//   return result;
+// }
+
+// const getSum2 = function (a, b) {
+//   return a + b;
+// };
+// console.log(getSum2(3, 4));
+
+// const getSum = (a, b) => {
+//   return a + b;
+// };
+
+// const getSum3 = (a) => a + 2;
+
+// const getSum4 = () => 2 + 2;
+
+// const redirect = (path) => {
+
+// }
+
+// console.log(getSum4());
+
+// (function () {
+//   console.log("Моментальний виклик");
+// })();
+
+// function greeting(name) {
+//   console.log(`Привіт, ${name}!`);
+
+//   return `Привіт, ${name}!`;
+// }
+
+// function greeting2(name) {
+//   alert(`Привіт, ${name}!`);
+// }
+
+// function processUserInput(callback) {
+//   const userName = prompt("Введіть своє ім'я");
+
+//   callback(userName);
+// }
+
+// // processUserInput(greeting2);
+// processUserInput(function (name) {
+//   alert(`Привіт, ${name}!`);
+// });
+
+// function validate(input, cb) {
+//   if (input.includes("@")) {
+//   } else {
+//   }
+// }
+
+// function pow(x, n) {
+//   // x = 2, n = 3
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     // result *= x
+//     result = result * x;
+//   }
+
+//   return result;
+// }
+
+// function pow(x, n) {
+//   if (n === 1) {
+//     return x;
+//   }
+
+//   return x * pow(x, n - 1);
+// }
+
+// const number = pow(2, 3);
+// console.log("number:", number);
+
+// for (let i = 0; i < 10; i++) {
+//   for(let j = 0; j < 10; i++) {
+//     for () {
+
+//     }
+//   }
 // }
