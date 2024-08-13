3e6292387b6c58704da0bbfdb08ccce55611e2f5
Mykola Hubytskyi <mykolahubytskyi@MacBook-Pro-Mykola.local>
2024-08-13 21:01:24 +0300

    add set/map

--- app.js
+++ app.js
@@ -1,122 +1,215 @@
-"use strict";
+// "use strict";
 
-var message;
+// const numbers = [1, 2, 3];
+// const [firstNumber, secondNumber, thirdNumber] = numbers;
 
-// let message = "Hello World!";
-// message = 42;
-
-// const message = "Hello!";
-// message = 1;
-// console.log(message);
-
-// const user = "John";
-// const User = "Jane";
-
-// console.log(user);
-// console.log(User);
-
-// const num = 123;
-// const userName = "Jane";
-// let userName = 123
-// const API_URL = "";
-// const userFirstName = "Jane";
-
-// const user_name =
-
-// const years = 30 - 5;
-
-// console.log(Infinity);
-
-// console.log(BigInt(42) + 2n);
-
-// const name = "Анна";
-
-// console.log("Привіт, " + name);
-// console.log(`Привіт, ${name}`);
-
-// console.log(true);
-// console.log(false);
+// console.log(firstNumber, secondNumber, thirdNumber)
 
 // const user = {
-//   name: "Анна",
-//   age: 20,
-//   isActive: true,
-//   hasPets: false,
-//   address: {
-//     city: "Kyiv",
-//     zip: 12345,
-//   },
+//     name: "Юлія",
+//     age: 20
 // };
 
-// let user = null;
-
-// logic ...
-
-// user = {};
-// console.log(user);
-
-// if (typeof user === "object" && user !== null) {
+// for (const [key, value] of Object.entries(user)) {
+//     console.log(key, value)
 // }
 
-// const number = 11;
+// let guest = 'Jane';
+// let admin = 'Pete';
 
-// console.log(number % 2);
+// [guest, admin] = [admin, guest];
 
-// console.log(typeof null);
+// console.log('admin: ', admin)
+// console.log('guest: ', guest)
 
-// console.log(10 <= 10);
-// console.log(5 >= 10);
-// console.log(15 >= 10);
+// const numbers = [1, 2, 3, 4, 5, 6];
 
-// const isAdmin = true;
-// const hasPets = false;
-// const age = 17;
+// const [first, second, ...restNumbers] = numbers;
 
-// console.log(isAdmin || hasPets || age >= 18);
+// console.log('first: ', first);
+// console.log('second: ', second);
+// console.log('restNumbers: ', restNumbers);
 
-// console.log(!!"q");
+// const title = "Footer Menu";
 
-// let user1 = "Анна";
-// const user2 = user1;
-// user = "Анна";
+// const options = {
+//     title: 'Menu',
+//     width: 100,
+//     height: 200
+// }
 
-// user1 = "Юлія";
-// console.log(user1);
-// console.log(user2);
+// const { height, title: newTitle, width } = options;
 
-// adiu3y4h23xb6g2x63g2xdg22
+// console.log('title: ', options.title)
+
+// console.log('title: ', title)
+// console.log('newTitle: ', newTitle)
+// console.log('width: ', width)
+// console.log('height: ', height)
+
+const options = {
+    title: 'Menu',
+    size: {
+        width: 100,
+        height: 200
+    },
+    items: ['header', 'footer']
+}
+
+const { size: { width: w, height: h }, items: [first, second] } = options;
+
+// console.log('width: ', w)
+// console.log('height: ', height)
+// console.log('first: ', first)
+// console.log('second: ', second)
+
+// const { title, ...otherOptions } = options;
+// console.log(title)
+// console.log(otherOptions)
+
+// function sumAll(one, two, ...args) {
+//     let sum;
+
+//     console.log('argument: ', args)
+// }
+
+// sumAll(1)
+// sumAll(1, 2)
+// sumAll(1, 2, 3)
+// const numbers = [3, 4, 1]
+// console.log(numbers)
+// console.log(...numbers)
+// console.log(Math.max(...numbers))
+
+// const arr = [1, 2, 3, 4]
+// const obj = { a: 1, b: 2 }
+
+// const newArr = [...arr]
+// const newObj = { ...obj }
+// newObj.b = 5
+
+// console.log(arr)
+// console.log(obj)
+// console.log(newArr)
+// console.log(newObj)
+
+// const set = new Set();
+// const set2 = new Set(['апельсини', 'ананаси', 'апельсини', 'яблука']);
+
+// set2.add(111)
+// set2.add(222)
+// set2.add(333)
+// set2.add(111)
+
+// console.log(set2.size)
+// console.log(set2.delete(111))
+// set2.clear()
+
+// console.log(set2.size)
+
+// const someArr = [1, 2, 3, 4, 5, 6];
+// const set = new Set([1, 2, 3, 4, 5, 6])
+
+// someArr.forEach(function (number, index, arr) {
+//     console.log('index: ', index, number, arr);
+// })
+
+// set.forEach(function (value, valueAgain, set) {
+//     console.log(value, valueAgain, set);
+// })
+
+// const a = { a: 1 }
+// const set = new Set([a, a])
+// console.log(set)
+
+// const set = new Set([1, '1'])
+// console.log(set)
+
 // const user = {
-//   name: "Анна",
-// };
+//     name: 'John',
+//     age: 42
+// }
 
-// // iu3axdsdasdg2xdg22
-// const user3 = {
-//   name: "Анна",
-// };
+// console.log(user)
+// console.log(Object.entries(user))
 
-// console.log(user === user3);
+// const map = new Map([
+//     ['name', 'Jane'],
+//     ['age', 30]
+// ])
 
-// const user2 = user; // user = adiu3y4h23xb6g2x63g2xdg22
+// map.set(1, 'Значення по ключу числовому')
+// map.set('1', 'Значення по ключу рядковому')
+// map.set(true, 'Значення по ключу булевому')
 
-// user2.name = "Юлія";
+// console.log(map)
 
-// console.log(user === user2);
-// console.log(user2);
+// for (const key of map.keys()) {
+//     console.log(key)
+// }
 
-// console.log("---> ", user1, user2);
-// console.log(user2);
+// for (const value of map.values()) {
+//     console.log(value)
+// }
 
-// let message = 1;
-// let message = 2;
+// for (const [key, value] of map) {
+//     console.log(key, value)
+// }
 
-// var message = 1;
-// var message = 2;
+// map.forEach(function (key, value, map) {
+//     console.log(key, value, map);
+// })
 
-// console.log(message); // undefined
-// console.log(message2); // Error
-// let message2 = 5;
-// var message = 10;
-// console.log(message); // 10
-// console.log(message2); // 5
+// const user = {
+//     1: 'number',
+//     '42': 'string',
+//     true: 'bool'
+// }
 
-console.log(typeof "1");
+// const someFunc = () => console.log('some text')
+
+// const map = new Map([
+//     [undefined, 'тут ключ undefined'],
+//     [someFunc, 'тут ключ функція']
+// ]);
+
+// console.log(map.get(someFunc))
+
+// const set = new Set([1, 2, 3, 4, 4, 4, 4])
+// console.log([...set])
+
+// let user = {
+//     name: 'jane'
+// }
+
+// const arr = [user];
+
+// console.log(arr);
+
+// const weakMap = new WeakMap()
+// weakMap.set(user, 'some text')
+
+// user = null
+
+// for (const entry of weakMap) {
+//     console.log(entry)
+// }
+// console.log(weakMap)
+
+// const arr = [1, 2, 3, 4, 5]
+
+// console.log(arr.map((number) => {
+//     return number + 10
+// }));
+
+// console.log(arr.filter((number) => number > 2))
+
+// const user = {
+//     firstName: 'Jane',
+//     lastName: 'Doe',
+//     getFullName() {
+//         return this.firstName + ' ' + this.firstName
+//     }
+// }
+
+// user.getFullName()
