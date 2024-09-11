b34c3950df09012d0f0f1a88fce7425464047fb5
Mykola Hubytskyi <mykolahubytskyi@MacBook-Pro-Mykola.local>
2024-09-11 08:49:05 +0300

    add claculator

--- app.js
+++ app.js
@@ -1,149 +1,160 @@
-"use strict";
+'use strict'; // Використання строгого режиму для забезпечення безпечнішого коду
 
-// 1. Додати нові задачі +
-// 2. Видаляти всі задачі +
-// 3. Видаляти окремі задачі +
-// 4. Пошук/фільтрація +
-// 5. Зберігати в стореджі +
+// Отримуємо елементи DOM для взаємодії з ними
+const input = document.querySelector('.input'); // Поле для відображення чисел і операцій
+const operators = document.querySelectorAll('.operator'); // Кнопки операторів (+, -, ×, ÷)
+const numbers = document.querySelectorAll('.number'); // Кнопки чисел
+const clear = document.querySelector('.clear'); // Кнопка очищення
+const result = document.querySelector('.result'); // Кнопка для відображення результату
 
-// Отримуємо необхідні DOM елементи
-const form = document.querySelector('.create-task-form'); // Форма для створення задач
-const taskInput = document.querySelector('.task-input'); // Поле для введення задачі
-const collection = document.querySelector('.collection'); // Список задач
-const clearButton = document.querySelector('.clear-tasks'); // Кнопка для видалення всіх задач
-const filterInput = document.querySelector('.filter-input'); // Поле для фільтрації задач
+const RESULT_KEY = 'result'; // Ключ для збереження результату в LocalStorage
+const operatorsList = ['+', '-', '×', '÷']; // Список операторів для перевірки
+let isResultDisplay = false; // Стан, що вказує на відображення результату
 
-// Ключ для збереження задач в LocalStorage
-const TASKS_STORAGE_KEY = 'tasks';
+// Функція для обробки кліку на оператор
+function handleOperatorClick(event) {
+    const operatorValue = event.target.textContent; // Отримуємо значення оператора
+    const lastChar = input.textContent.at(-1); // Останній символ у полі відображення
 
-// Додаємо обробники подій
-document.addEventListener('DOMContentLoaded', renderTasks); // При завантаженні сторінки відображаємо задачі з LocalStorage
-form.addEventListener('submit', addTask); // Додаємо задачу при сабміті форми
-clearButton.addEventListener('click', removeTasks); // Видаляємо всі задачі при натисканні на кнопку
-collection.addEventListener('click', removeTask); // Видаляємо окрему задачу при натисканні на кнопку "x"
-filterInput.addEventListener('input', filterTasks); // Фільтруємо задачі при введенні тексту у поле
-
-// Відображення задач з LocalStorage
-function renderTasks() {
-    if (getTasksFromLocalStorage()) {
-        const tasks = JSON.parse(getTasksFromLocalStorage());
-
-        // Створюємо елемент для кожної задачі
-        tasks.forEach((task, index) => {
-            createTask(task, index);
-        });
-    }
-}
-
-// Отримуємо індекс останньої задачі для присвоєння новим задачам
-function getLastTasksIndex() {
-    if (getTasksFromLocalStorage()) {
-        return JSON.parse(getTasksFromLocalStorage()).length;
+    // Якщо поле відображення містить '0', операція не виконується
+    if (input.innerHTML === '0') {
+        return null;
     }
 
-    return 0;
-}
-
-// Створення HTML-елементу для задачі
-function createTask(task, index) {
-    const li = document.createElement('li');
-    li.innerHTML = task;
-    li.classList.add('task');
-    li.setAttribute('data-id', index); // Присвоюємо задачі унікальний індекс
-
-    // Додаємо кнопку для видалення задачі
-    const button = document.createElement('button');
-    button.innerHTML = 'x';
-    button.className = 'button-icon button-delete';
-
-    li.append(button);
-
-    // Додаємо задачу до колекції (списку)
-    collection.append(li);
-}
-
-// Додавання нової задачі
-function addTask(event) {
-    event.preventDefault(); // Відміняємо перезавантаження сторінки при сабміті
-
-    const currentForm = event.target;
-    const inputValue = currentForm.task.value; // Отримуємо значення з поля вводу
-
-    if (!inputValue) {
-        return; // Якщо поле порожнє, не додаємо задачу
+    // Якщо останній символ є оператором, замінюємо його на новий оператор
+    if (operatorsList.includes(lastChar)) {
+        const inputValueWithoutOperator = input.innerHTML.substring(0, input.innerHTML.length - 1); // Видаляємо останній оператор
+        input.innerHTML = inputValueWithoutOperator + operatorValue; // Додаємо новий оператор
+    } else {
+        input.innerHTML += operatorValue; // Якщо операторів немає, додаємо оператор до рядка
     }
 
-    const currentIndex = getLastTasksIndex(); // Отримуємо індекс для нової задачі
-
-    createTask(inputValue, currentIndex); // Створюємо нову задачу
-    setTaskToLocalStorage(inputValue); // Зберігаємо задачу в LocalStorage
-
-    currentForm.reset(); // Очищаємо поле вводу
+    isResultDisplay = false; // Очищаємо стан відображення результату
 }
 
-// Зберігаємо задачу в LocalStorage
-function setTaskToLocalStorage(task) {
-    let tasks = [];
+// Функція для обробки кліку на число
+function handleNumberClick(event) {
+    const numberValue = event.target.textContent; // Отримуємо значення числа
+    const inputValue = input.innerHTML; // Поточний рядок у полі відображення
 
-    if (getTasksFromLocalStorage()) {
-        tasks = JSON.parse(getTasksFromLocalStorage()); // Отримуємо існуючі задачі
+    const numbersOnly = inputValue.split(/\+|\-|\×|\÷/g); // Отримуємо числа без операторів
+
+    // Перевіряємо, чи вже є крапка в числі
+    if (numberValue === '.' && numbersOnly.at(-1).includes('.')) {
+        return null; // Якщо є, більше не додаємо крапку
     }
 
-    tasks.push(task); // Додаємо нову задачу
-    setTasksToLocalStorage(tasks); // Оновлюємо LocalStorage
-}
-
-// Оновлюємо LocalStorage
-function setTasksToLocalStorage(tasks) {
-    localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks)); // Зберігаємо задачі у форматі JSON
-}
-
-// Отримуємо задачі з LocalStorage
-function getTasksFromLocalStorage() {
-    return localStorage.getItem(TASKS_STORAGE_KEY); // Повертаємо задачі у вигляді рядка
-}
-
-// Очищаємо LocalStorage
-function clearTasksFromLocalStorage() {
-    localStorage.removeItem(TASKS_STORAGE_KEY); // Видаляємо всі задачі з LocalStorage
-}
-
-// Видалення всіх задач
-function removeTasks() {
-    collection.innerHTML = ''; // Очищаємо HTML-код списку задач
-
-    clearTasksFromLocalStorage(); // Очищаємо LocalStorage
-}
-
-// Видалення окремої задачі
-function removeTask(event) {
-    if (event.target.classList.contains('button-delete')) {
-        const li = event.target.closest('.task'); // Знаходимо батьківський елемент (задачу)
-        const tasks = JSON.parse(getTasksFromLocalStorage()); // Отримуємо задачі з LocalStorage
-
-        // Фільтруємо задачі, залишаючи лише ті, які не мають індексу видаленої задачі
-        const filteredTasks = tasks.filter((_, index) => {
-            return index.toString() !== li.getAttribute('data-id');
-        });
-
-        removeTasks(); // Видаляємо всі задачі з інтерфейсу
-        setTasksToLocalStorage(filteredTasks); // Зберігаємо оновлений список задач
-        renderTasks(); // Відображаємо задачі після видалення
+    // Якщо введене число є 0 або відображається результат, перезаписуємо поле відображення
+    if (input.innerHTML === '0' || isResultDisplay) {
+        input.innerHTML = numberValue;
+    } else {
+        input.innerHTML += numberValue; // Додаємо число до рядка
     }
+
+    isResultDisplay = false; // Очищаємо стан відображення результату
 }
 
-// Фільтрація задач
-function filterTasks(event) {
-    const filterQuery = event.target.value; // Отримуємо значення з поля фільтрації
-
-    const tasks = collection.querySelectorAll('.task'); // Отримуємо всі задачі
-
-    tasks.forEach((task) => {
-        const taskValue = task.firstChild.textContent; // Отримуємо текст задачі
-        if (!taskValue.includes(filterQuery.trim())) {
-            task.classList.add('hidden'); // Ховаємо задачі, які не відповідають запиту
-        } else {
-            task.classList.remove('hidden'); // Показуємо задачі, що відповідають запиту
-        }
-    });
+// Функція для очищення поля відображення
+function handleClear() {
+    input.innerHTML = '0'; // Скидаємо значення на '0'
+    clearResultFromLS(); // Очищаємо результат у LocalStorage
 }
+
+// Функція для обробки кліку на кнопку результату
+function handleResult() {
+    const inputValue = input.innerHTML; // Отримуємо поточне значення
+
+    if (inputValue === '0') {
+        return null; // Якщо значення '0', не обробляємо
+    }
+
+    const numbersOnly = inputValue.split(/\+|\-|\×|\÷/g); // Отримуємо числа
+    const operatorsOnly = inputValue.replace(/[0-9]|\./g, "").split(''); // Отримуємо оператори
+
+    // Ділення
+    let dividerIndex = operatorsOnly.indexOf('÷'); // Індекс оператора ділення
+
+    while (dividerIndex !== -1) {
+        const result = numbersOnly[dividerIndex] / numbersOnly[dividerIndex + 1]; // Ділимо числа
+
+        numbersOnly.splice(dividerIndex, 2, result); // Замінюємо два числа результатом
+        operatorsOnly.splice(dividerIndex, 1); // Видаляємо оператор
+
+        dividerIndex = operatorsOnly.indexOf('÷'); // Шукаємо наступне ділення
+    }
+
+    // Множення
+    let multiplyIndex = operatorsOnly.indexOf('×'); // Індекс оператора множення
+
+    while (multiplyIndex !== -1) {
+        const result = numbersOnly[multiplyIndex] * numbersOnly[multiplyIndex + 1]; // Множимо числа
+
+        numbersOnly.splice(multiplyIndex, 2, result); // Замінюємо два числа результатом
+        operatorsOnly.splice(multiplyIndex, 1); // Видаляємо оператор
+
+        multiplyIndex = operatorsOnly.indexOf('×'); // Шукаємо наступне множення
+    }
+
+    // Віднімання
+    let subIndex = operatorsOnly.indexOf('-'); // Індекс оператора віднімання
+
+    while (subIndex !== -1) {
+        const result = numbersOnly[subIndex] - numbersOnly[subIndex + 1]; // Віднімаємо числа
+
+        numbersOnly.splice(subIndex, 2, result); // Замінюємо два числа результатом
+        operatorsOnly.splice(subIndex, 1); // Видаляємо оператор
+
+        subIndex = operatorsOnly.indexOf('-'); // Шукаємо наступне віднімання
+    }
+
+    // Додавання
+    let sumIndex = operatorsOnly.indexOf('+'); // Індекс оператора додавання
+
+    while (sumIndex !== -1) {
+        const result = Number(numbersOnly[sumIndex]) + (Number(numbersOnly[sumIndex + 1])); // Додаємо числа
+
+        numbersOnly.splice(sumIndex, 2, result); // Замінюємо два числа результатом
+        operatorsOnly.splice(sumIndex, 1); // Видаляємо оператор
+
+        sumIndex = operatorsOnly.indexOf('+'); // Шукаємо наступне додавання
+    }
+
+    input.innerHTML = numbersOnly[0]; // Виводимо результат
+    setResultToLS(numbersOnly[0]); // Зберігаємо результат в LocalStorage
+
+    isResultDisplay = true; // Встановлюємо стан відображення результату
+}
+
+// Зберігає результат у LocalStorage
+function setResultToLS(result) {
+    localStorage.setItem(RESULT_KEY, result);
+}
+
+// Виводить результат із LocalStorage під час завантаження сторінки
+function displayResultFromLS() {
+    input.innerHTML = localStorage.getItem(RESULT_KEY) ?? '0'; // Встановлюємо 0, якщо результату немає
+}
+
+// Очищає результат у LocalStorage
+function clearResultFromLS() {
+    localStorage.removeItem(RESULT_KEY); // Видаляє результат з LocalStorage
+}
+
+// Подія при завантаженні сторінки для відображення попереднього результату
+document.addEventListener('DOMContentLoaded', displayResultFromLS);
+
+// Додає обробники подій для кожного оператора
+operators.forEach((operator) => {
+    operator.addEventListener('click', handleOperatorClick);
+});
+
+// Додає обробники подій для кожного числа
+numbers.forEach((number) => {
+    number.addEventListener('click', handleNumberClick);
+});
+
+// Очищення результату при натисканні на кнопку "очистити"
+clear.addEventListener('click', handleClear);
+
+// Виведення результату при натисканні на кнопку "результат"
+result.addEventListener('click', handleResult);
