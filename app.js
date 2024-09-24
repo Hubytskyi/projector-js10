30537ee8b31c27b3777e14f4d706a3a2e8fc708f
Mykola Hubytskyi <mykolahubytskyi@MacBook-Pro-Mykola.local>
2024-09-24 20:43:48 +0300

    add promises

--- app.js
+++ app.js
@@ -1,132 +1,78 @@
 "use strict";
 
-// Task 1
-// console.log(a);  // ? 10, undefined
-// var a = 10;
-
-// function test() {
-//   console.log(b);  // ? 20, undefined
-//   var b = 20;
-
-//   if (true) {
-//     var b = 30;
-//     console.log(b);  // ? 30
-//   }
-
-//   console.log(b);  // ? 30
-// }
-
-// test();
-
-
-// Task 2
-// function makeCounter() {
-//   let count = 0;
-
-//   return function () {
-//     count += 1;
-//     return count;
-//   };
-// }
-
-// const counter1 = makeCounter();
-// const counter2 = makeCounter();
-
-// console.log(counter1());  // ? 1
-// console.log(counter1());  // ? 2
-// console.log(counter2());  // ? 1
-
-// const numbers = [1, 2, 3, 4, 5];
-
-// function numberMap(n) {
-//   if (n % 2 === 0) {
-//     return n * 2;
-//   }
-
-//   return n;
-// }
-
-// const result = numbers.map(n => {
-//   if (n % 2 === 0) {
-//     return n * 2;
-//   }
-
-//   return n;
+// const promise = new Promise(function (resolve, reject) {
+//     resolve(123)
 // });
 
-// console.log(result); // ? [1,4,3,8,5] +++
-// console.log(result); // ? [4, 8] +
-// console.log(result); // ? [empty, 4, empty,  8, empty] +
-// console.log(result); // ? [undefined, 4, undefined,  8, undefined] +
+// fetch('api url')
+//     .then
+// // console.log(promise)
 
-// "use strict";
-// console.log(this)
-// const person = {
-//   name: 'Alice',
-//   age: 25,
-//   greet() {
-//     // return this;
-//     // return `Hello, my name is ${this.name}`;
-//   }
-// };
+// promise.then((result) => {
+//     console.log(result)
+// })
 
-// console.log(person.greet())
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((users) => {
+//         console.log(users)
+//     })
 
-// const greetFn = person.greet;
-// const greetFn = person.greet.bind(person);
-// console.log(greetFn());  // ? 
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((posts) => {
+//         console.log(posts)
+//     })
 
-// `Hello, my name is undefined`
-// `Hello, my name is Alice`
-// function, obejct
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//     method: "PUT",
+//     body: JSON.stringify({
+//         title: "Test post",
+//         body: "some text...",
+//     })
+// })
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((posts) => {
+//         console.log(posts)
+//     })
 
-// const [first, , third] = [1, 2, 3, 4, 5];
 
-// console.log(first);  // ? 1
-// console.log(third);  // ? 3
 
-// console.log('start')
-// setTimeout(() => {
-//   console.log('settimeout')
-// }, 3000)
-// console.log('end')
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response) => {
+//         console.log(response)
 
-// function first() {
-//   console.log('1')
-//   second()
+//         if (!response.ok) {
+//             throw new Error('Something went wrong!')
+//         }
+
+//         return response.json()
+//     })
+//     .then((posts) => {
+//         console.log(posts)
+//         renderPosts(posts)
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
+
+// function renderPosts(posts) {
+//     const list = document.querySelector('ul');
+
+//     posts.forEach((post) => {
+//         const li = document.createElement('li')
+//         li.innerHTML = post.title;
+
+//         list.append(li)
+//     })
 // }
 
-// function second() {
-//   console.log(2)
-// }
-
-// first();
-
-// []
-// [first]
-// [first, log('1')]
-// [first]
-// [first, second]
-// [first, second, log(2)]
-// [first, second]
-// [first]
-// []
-
-// function rec() {
-//   // повинен бути вихід з рекурсії
-//   rec();
-// }
-
-// rec()
-
-// []
-// [rec]
-// [rec, rec,]
-// [rec, rec, rec]
-// [rec, rec, rec, rec]
-
-// const button = document.querySelector('button')
-// button.addEventListener('click', handleClick)
-// function handleClick() {
-//   console.log('click')
+// const user = {
+//     "id": 1
 // }
